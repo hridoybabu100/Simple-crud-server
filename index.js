3ee26319b292b0b41caf96cae063b40e73e754b9
Hridoy Hasan Babu <hridoyakanda100@gmail.com>
2026-05-08 00:38:58 +0600

    again updated

--- index.js
+++ index.js
@@ -9,16 +9,15 @@
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
 // })
-const dns = require('node:dns').promises;
-dns.setServers(['8.8.8.8', '8.8.4.4']);
-
+const dns = require("node:dns").promises;
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // Adds headers: Access-Control-Allow-Origin: *
 app.use(cors());
@@ -43,6 +42,42 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //  const database = client.db("sample_mflix");
+    // const movies = database.collection("movies");
+
+    const db = client.db("UserCrud");
+    const userCollection = db.collection("Users");
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/users/:id", async (req, res) => {
+      // console.log(req.params);
+      const id = req.params.id;
+      const quary = {
+        _id: new ObjectId(id),
+      };
+
+      const user = await userCollection.findOne(quary);
+      res.send(user);
+    });
+
+
+    app.delete("/users/:id", async (req, res) => {
+      // console.log(req.params);
+      const id = req.params.id;
+      const quary = {
+        _id: new ObjectId(id),
+      };
+
+      const result = await userCollection.deleteOne(quary);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
