e297b1379edbf2d024305e71da3c159f334b6784
Hridoy Hasan Babu <hridoyakanda100@gmail.com>
2026-05-07 13:19:29 +0600

    again updated

--- index.js
+++ index.js
@@ -9,11 +9,14 @@
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
 // })
+const dns = require('node:dns').promises;
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 5000;
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
@@ -25,42 +28,8 @@ app.use(express.json());
 //
 //
 
-const uri = `mongodb+srv://SimpleDBUser:870uTtY6OBd2PXF9@cluster0.gkrxgec.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://userServer:r4xLhFm952sdGanh@cluster0.gkrxgec.mongodb.net/?appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-
-// mongodb+srv://SimpleDBUser:iipcCerq9oAw25oo@cluster0.gkrxgec.mongodb.net/?appName=Cluster0
-
-// mongodb+srv://<db_username>:<db_password>@cluster0.gkrxgec.mongodb.net/?appName=Cluster0
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-
-
-app.get("/", (req, res) => {
-  res.send("Hello simple express js server");
-});
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -81,12 +50,14 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
+app.get("/", (req, res) => {
+  res.send("Hello simple express js server");
+});
 
 app.listen(port, () => {
   console.log(`Simple app express js data server ${port}`);
